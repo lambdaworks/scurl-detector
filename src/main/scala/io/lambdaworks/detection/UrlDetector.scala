67e83d3f097cb917885cb91f2ddf5b4fb29700be
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2021-06-17 10:33:10 +0200

    Improve 'sanitize' method

--- src/main/scala/io/lambdaworks/detection/UrlDetector.scala
+++ src/main/scala/io/lambdaworks/detection/UrlDetector.scala
@@ -1,24 +1,24 @@
 package io.lambdaworks.detection
 
-import scala.jdk.CollectionConverters._
 import com.linkedin.urls.detection.{UrlDetector => LUrlDetector, UrlDetectorOptions => LUrlDetectorOptions}
 import com.linkedin.urls.{Url => LUrl}
 import org.apache.commons.lang3.StringUtils.endsWithAny
 
 import scala.annotation.tailrec
+import scala.jdk.CollectionConverters._
 
 final case class UrlDetector(content: String, options: UrlDetectorOptions = UrlDetectorOptions.Default) {
 
   private val detector: LUrlDetector = new LUrlDetector(content, LUrlDetectorOptions.valueOf(options.value))
 
-  def extract(): List[Url] = detector.detect().asScala.toList.map(Url.apply).map(sanitize)
+  def extract(): List[Url] = detector.detect().asScala.toList.map(sanitize(_))
 
-  private def sanitize(url: Url): Url = {
+  private def sanitize(url: String): Url = {
     @tailrec
-    def loop(urlString: String): String =
-      if (!endsWithAny(urlString, ",", "!", "-", ".", "`")) urlString
-      else loop(urlString.substring(0, urlString.length - 1))
-    Url(LUrl.create(loop(url.toString)))
+    def loop(url: String): String =
+      if (!endsWithAny(url, ",", "!", "-", ".", "`")) url else loop(url.substring(0, url.length - 1))
+
+    Url(LUrl.create(loop(url)))
   }
 
 }
