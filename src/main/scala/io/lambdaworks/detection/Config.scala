b9eb159f419a9b77a55aa5fd51522547baf4f69a
LeonaNedeljkovic <leona.nedeljkovic@gmail.com>
2021-06-28 08:42:50 +0200

    Add scaladoc

--- src/main/scala/io/lambdaworks/detection/Config.scala
+++ src/main/scala/io/lambdaworks/detection/Config.scala
@@ -1,5 +1,11 @@
 package io.lambdaworks.detection
 
+/** Represents configuration of URL detector.
+  *
+  *  @param options detector options
+  *  @param allowlist list of allowed URLs
+  *  @param denylist list of forbidden URLs
+  */
 final case class Config(
   options: UrlDetectorOptions = UrlDetectorOptions.Default,
   allowlist: List[String] = Nil,
