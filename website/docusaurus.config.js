84d1ad2ac714685113326bb6409a559ce5045579
mvelimir <velimirmilinkovic1@gmail.com>
2022-08-23 12:02:34 +0200

    Add custom CSS for API

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -28,7 +28,10 @@ const config = {
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: [
+            require.resolve('./src/css/custom.css'),
+            require.resolve('./api/')
+          ]
         }
       })
     ]
