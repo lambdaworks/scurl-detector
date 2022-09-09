ecd1923db610400185c42448aec4c5e143af41b8
Scala Steward <scala_steward@virtuslab.com>
2022-09-09 15:43:34 +0000

    Update scalatest to 3.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private object Versions {
 
     val UrlDetector           = "0.1.23"
-    val ScalaTest             = "3.2.12"
+    val ScalaTest             = "3.2.13"
     val CommonsValidator      = "1.7"
     val ScalaCollectionCompat = "2.8.1"
     val ScalaUri              = "4.0.2"
