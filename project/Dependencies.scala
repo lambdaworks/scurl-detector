83ce0a42cc0d6d31ce76f3f8cc309476029af653
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-01-11 17:12:42 +0100

    Update scalatest to 3.2.15 (#68)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private object Versions {
 
     val UrlDetector           = "0.1.23"
-    val ScalaTest             = "3.2.14"
+    val ScalaTest             = "3.2.15"
     val CommonsValidator      = "1.7"
     val ScalaCollectionCompat = "2.9.0"
     val ScalaUri              = "4.0.3"
