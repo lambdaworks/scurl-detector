e7ea5f5bb78b8b59e46dbe0e1f7961fead45c46d
Scala Steward <scala_steward@virtuslab.com>
2025-07-09 19:04:48 +0000

    Update commons-validator to 1.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
     val UrlDetector           = "0.1.23"
     val ScalaTest             = "3.2.19"
-    val CommonsValidator      = "1.9.0"
+    val CommonsValidator      = "1.10.0"
     val ScalaCollectionCompat = "2.13.0"
     val ScalaUri              = "4.2.0"
     val OrganizeImports       = "0.6.0"
